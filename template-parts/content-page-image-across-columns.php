2165eee8123e4976c824a7dcee2bc8215f7147b5
Jeff Falcon <jeff@tankadesign.com>
2020-01-13 18:14:03 -0500

    initial section render

---
+++ template-parts/content-page-image-across-columns.php
@@ -0,0 +1,42 @@
+<section class="image-across-columns">
+	<div class="inner">
+    <?php
+      if(have_rows('location-items')) :
+        $image = get_sub_field('image');
+        $columns = (int) get_sub_field('image-columns');
+        $position = get_sub_field('image-position');
+        $fill = get_sub_field('image-fill');
+        $title = get_sub_field('image-title');
+        $body = get_sub_field('image-body');
+        $valign = get_sub_field('text-valign');
+        $imgStyles = [];
+        $txtStyles = [];
+        $imgFillStyles = [];
+        $maxCols = 4;
+        $imgStyles[] = 'flex: ' . $columns;
+        $txtStyles[] = 'flex: ' . $maxCols - $columns;
+        $imgStyles[] = 'order: ' . ($position == 'left' ? 1 : 2);
+        $txtStyles[] = 'order: '. ($position == 'left' ? 2 : 1);
+        $txtStyles[] = 'align-self: ' . $valign;
+        $imgStylesBg = 'background: url("' . $image['url'] . '") center center no-repeat';
+        $imgStylesBg .= '; background-size: ' . ($fill === 'contain' ? 'contain' : 'cover');
+        if($fill === 'cover' || $fill === 'contain') :
+          $imgStyles[] = $imgStylesBg;
+        else :
+          $imgFillStyles[] = $imgStylesBg;
+        endif;
+    ?>
+    <div class="image <?= $fill ?>" style="<?= implode('; ', $imgStyles) ?>">
+      <?php if($fill == 'cover-padded') :?>
+        <div class="fill" style="<?= implode('; ', $imgFillStyles) ?>"></div>
+      <?php endif; ?>
+    </div>
+    <div class="text" <?= $txtStyle?>>
+      <h2 class="title"><?= $title ?></h2>
+      <div class="body"><?= $body ?></div>
+    </div>
+    <?php
+      endif;
+    ?>
+	</div>
+</section>
---
+++ template-parts/content-page-locations.php
@@ -0,0 +1,32 @@
+<section class="locations">
+	<div class="inner">
+    <?php
+      if(have_rows('location-items')) :
+        $sectionTitle = get_sub_field('section-title');
+    ?>
+		<div class="locations">
+			<?php
+          $i = 0;
+					while(have_rows('location-items')) : the_row();
+						$title = trim(get_sub_field('location-title'));
+						$description = trim(get_sub_field('location-description'));
+            $image = get_sub_field('image');
+            $i++;
+			?>
+			<div class="location">
+      <?php if($i===1) : ?>
+        <h2 class="section-title"><?= $sectionTitle ?></h2>
+      <?php endif;?>
+				<div class="image" style="background-image: url('<?= $image['url'] ?>')"></div>
+				<h3 class="title"><?= $title ?></h3>
+				<p class="description"><?= $description ?></p>
+			</div>
+			<?php
+					endwhile;
+			?>
+		</div>
+			<?php
+				endif;
+			?>
+	</div>
+</section>
