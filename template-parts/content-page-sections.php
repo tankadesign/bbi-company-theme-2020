86e87de937cf9751e9d52ee7c85754441a15bf4e
Jeff Falcon <jeff@tankadesign.com>
2020-01-14 14:22:19 -0500

    last section class

--- template-parts/content-page-sections.php
+++ template-parts/content-page-sections.php
@@ -1,6 +1,7 @@
 <?php
   global $postCount;
   global $maxPosts;
+  global $isLastSection;
 
   if( have_rows('sections') ):
     while ( have_rows('sections') ) : the_row();
@@ -11,6 +12,7 @@
       $enabled = get_sub_field('section-enabled');
       if(!$enabled) continue;
       $layout = get_row_layout();
+      $isLastSection = $postCount == $maxPosts - 1;
       get_template_part( 'template-parts/content', 'page-' . $layout );
       $postCount++;
     endwhile;
