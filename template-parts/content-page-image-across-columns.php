060d2db3b5ec7fefa5bc38575ebd34ff4334dbc0
Jeff Falcon <jeff@tankadesign.com>
2020-01-23 00:19:24 -0500

    fix for when columns = 4 on image-across-columns

--- template-parts/content-page-image-across-columns.php
+++ template-parts/content-page-image-across-columns.php
@@ -1,25 +1,25 @@
 <?php
   global $isLastSection;
+  $image = get_sub_field('image');
+  $columns = (int) get_sub_field('image-columns');
+  $position = get_sub_field('image-position');
+  $fill = get_sub_field('image-fill');
+  $title = get_sub_field('image-title');
+  $body = get_sub_field('image-body');
+  $valign = get_sub_field('text-valign');
+  $imgStyles = [];
+  $txtStyles = [];
+  $imgFillStyles = [];
+  $maxCols = 4;
+  $imgStyles[] = 'flex: ' . $columns;
+  $txtStyles[] = 'flex: ' . ($maxCols - $columns);
+  $imgStyles[] = 'order: ' . ($position === 'left' ? 1 : 2);
+  $txtStyles[] = 'order: '. ($position === 'left' ? 2 : 1);
+  $txtStyles[] = 'display: flex; align-items: ' . $valign;
 ?>
-<section class="section-image-across-columns<?= $isLastSection ? ' last' : '' ?>">
+<section class="section-image-across-columns<?= $isLastSection ? ' last' : '' ?><?= $maxCols === $columns ? ' full' : '' ?>">
 	<div class="inner">
     <?php
-      $image = get_sub_field('image');
-      $columns = (int) get_sub_field('image-columns');
-      $position = get_sub_field('image-position');
-      $fill = get_sub_field('image-fill');
-      $title = get_sub_field('image-title');
-      $body = get_sub_field('image-body');
-      $valign = get_sub_field('text-valign');
-      $imgStyles = [];
-      $txtStyles = [];
-      $imgFillStyles = [];
-      $maxCols = 4;
-      $imgStyles[] = 'flex: ' . $columns;
-      $txtStyles[] = 'flex: ' . ($maxCols - $columns);
-      $imgStyles[] = 'order: ' . ($position === 'left' ? 1 : 2);
-      $txtStyles[] = 'order: '. ($position === 'left' ? 2 : 1);
-      $txtStyles[] = 'display: flex; align-items: ' . $valign;
     ?>
     <div class="image <?= $fill ?> <?= $position === 'left' ? 'first' : 'second' ?>" style="<?= implode('; ', $imgStyles) ?>">
       <img src="<?= esc_url($image['url']) ?>" />
