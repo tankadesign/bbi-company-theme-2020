4807955058cc32a22b8214f7a09e7d26e36725da
Jeff Falcon <jeff@tankadesign.com>
2021-03-01 19:06:25 -0500

    add Twist logo to footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
   <div class="logo-strip">
     <div class="inner">
       <?php
-        $footer_logos = ['ouidad', 'bioionic', 'wetbrush', 'goody', 'epic-pro', 'solano', 'ace'];
+        $footer_logos = ['ouidad', 'bioionic', 'wetbrush', 'goody', 'epic-pro', 'solano', 'twist', 'ace'];
         foreach ($footer_logos as $id) {
           include('assets/svg/' . $id . '-logo.svg');
         }
